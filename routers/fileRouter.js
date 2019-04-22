51415cfc51f0146a1da377d21f88e8a3fcc5be3b
Kendy Nguyen <kendynguyen20@hotmail.com>
2019-04-22 21:11:02 +0300

    changes on pug layout

--- routers/fileRouter.js
+++ routers/fileRouter.js
@@ -3,12 +3,12 @@ const express = require('express');
 const router = express.Router();
 const bodyParser = require('body-parser');
 const fileController = require('../controllers/fileController');
-const imageModel = require ('../models/fileUpload');
+const fileModel = require ('../models/fileUpload');
 const methodOverride = require('method-override');
 
 router.use(bodyParser.json());
 router.use(express.static('public'));
-router.use(methodOverride('_method'));  //for overriding post to get patch and delete
+router.use(methodOverride('_method'));  //for overriding post to get put and delete
 router.use(methodOverride('X-HTTP-Method'));
 router.use(methodOverride(function (req, res) {
     if (req.body && typeof req.body === 'object' && '_method' in req.body) {
@@ -32,6 +32,7 @@ router.get('/add', (req, res) => {
 
 router.get('/edit', (req, res) => {
     res.render('edit.pug');
+    console.log('in router.get edit');
 });
 
 router.get('/delete', (req, res) => {
@@ -51,10 +52,10 @@ router.post('/post', bodyParser.urlencoded({extended: true}), (req, res) => {
     res.send({type: 'POST from fileRouter'});
 });
 
-router.post('/edit', bodyParser.urlencoded({extended: true}), (req, res) => {
+router.post('/update', bodyParser.urlencoded({extended: true}), (req, res) => {
     console.log('reqbody: ' + req.body);
     console.log('reqbodyname: ' + req.body.name);
-    imageModel.create(req.body);
+    fileModel.create(req.body);
 
     File.find()
         .where('name').equals(req.body.name)
@@ -62,7 +63,7 @@ router.post('/edit', bodyParser.urlencoded({extended: true}), (req, res) => {
             id =>{
                 console.log('THIS IS THE ID' + id);
                 Cat.updateOne(
-                    imageModel.create(req.body)).then(c => {
+                    fileModel.create(req.body)).then(c => {
                     res.send('File edited: ' + req.body.name);
                 }, err => {
                     res.send('Error: ' + err);
@@ -110,7 +111,7 @@ router.use('/upload', function(req, res, next) {
 router.use('/upload', function(req, res){
     console.log(req.body);
     console.log(req.body.path + req.body.filename);
-    imageModel.create(req.body);
+    fileModel.create(req.body);
 
     const newFile = new File();
     newFile.img.data = fs.readFileSync(req.files.path);
@@ -123,16 +124,15 @@ router.use('/upload', function(req, res){
 
 
 // update by id---------------------------------------------------------------------------------------------------------
-router.put('/edit/:id', bodyParser.urlencoded({extended: true}), (req, res) => {
+router.put('/edit', bodyParser.urlencoded({extended: true}), (req, res) => {
     console.log('EDIT');
     console.log(req.body);
-    const id = req.params._id;
-    //const id = req.body._id;
+    const id = req.body._id;
     console.log(id);
-    imageModel.findByIdAndUpdate({_id: id}, req.body).then(function () {
-        imageModel.findOne({_id: req.params.id}).then(function (file) {
+    fileModel.findByIdAndUpdate({_id: id}, req.body).then(function () {
+        fileModel.findOne({_id: req.params.id}).then(function (file) {
             res.send(file);
-            imageModel.create(req.body);
+            fileModel.create(req.body);
         });
     });
     res.send({type: 'UPDATE'});
@@ -144,7 +144,7 @@ router.delete('/delete', bodyParser.urlencoded({extended: true}), (req, res) => 
     console.log(req.body);
     const id = req.body._id;
     console.log(id);
-    imageModel.findOneAndDelete({_id: id}).then(file => {
+    fileModel.findOneAndDelete({_id: id}).then(file => {
         res.redirect('/home');
         //res.send(file);
         //res.sendStatus(200);
